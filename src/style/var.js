41ede9ec9a665b91d1e3db4a971f3ae05483893a
Alexey Kosolapov <klealekle@gmail.com>
2016-05-19 09:20:22 +0300

    bar

---
+++ src/style/var.js
@@ -0,0 +1,34 @@
+/** Example
+module.exports = {
+    // Breakpoints
+    "$S": "300px", 
+    "$SSM": "380px",
+    "$SM": "550px", 
+    "$toM": "767px",
+    "$M": "768px", 
+    "$XM": "950px", 
+    "$L": "1200px", 
+    "$XL": "1328px"
+};
+**/
+module.exports = {
+    // Colors
+    "$text-main": "#fff",
+    "$text-gray-1": "#5c5f62",
+    "$text-gray-2": "#6c6c6c",
+    "$text-gray-3": "#8f8f8f",
+
+    "$link-blue": "#2faee7",
+    "$link-blue-active": "#2faee7",
+    "$link-light-blue": "#a6c6cb",
+    "$link-light-blue-active": "#a6c6cb",
+  
+    "$body-bg": "#2b2b2b",
+    "$bg-main": "#33393c",
+    "$bg-sub": "#252a2c",
+    "$bg-blue": "#037db5",
+    "$bg-dropdown": "#31373a",
+  
+    "$green": "#44930a",
+    "$subgreen": "#e8f39a"
+};
